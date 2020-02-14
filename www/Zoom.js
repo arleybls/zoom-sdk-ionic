c4b22b5e225d72cfe47f504476fc058e19283252
arleybls <arleybls@hotmail.com>
2020-02-14 17:21:04 +0100

    Update Zoom.js

--- www/Zoom.js
+++ www/Zoom.js
@@ -1,5 +1,7 @@
-var exec = require('cordova/exec');
 var PLUGIN_NAME = "Zoom";
+
+// Empty constructor
+function ZoomUs() {};
 
 function callNativeFunction(name, args, success, error) {
     args = args || [];
@@ -7,53 +9,52 @@ function callNativeFunction(name, args, success, error) {
     error = error || function(){};
     exec(success, error, PLUGIN_NAME, name, args);
 
-}
-
-var zoom = {
-
-    initialize: function(appKey, appSecret, success, error) {
-        callNativeFunction('initialize', [appKey, appSecret], success, error);
-    },
-
-    login: function(username, password, success, error) {
-        callNativeFunction('login', [username, password], success, error);
-    },
-
-    logout: function(success, error) {
-        callNativeFunction('logout', [], success, error);
-    },
-
-    isLoggedIn: function(success, error) {
-        callNativeFunction('isLoggedIn', [], success, error);
-    },
-
-    joinMeeting: function(meetingNo, meetingPassword, displayName, options, success, error) {
-         callNativeFunction('joinMeeting', [meetingNo, meetingPassword, displayName, options], success, error);
-    },
-
-    startMeetingWithZAK: function(meetingNo, displayName, zoomToken, zoomAccessToken, userId, options, success, error) {
-        callNativeFunction('startMeeting', [meetingNo, displayName, zoomToken, zoomAccessToken, userId, options], success, error);
-    },
-
-    startMeeting: function(meetingNo, options, success, error) {
-        callNativeFunction('startMeeting', [meetingNo, "", "", "", "", options], success, error);
-    },
-
-    startInstantMeeting: function(options, success, error) {
-        callNativeFunction('startInstantMeeting', [options], success, error);
-    },
-
-    setLocale: function(languageTag, success, error) {
-        callNativeFunction('setLocale', [languageTag], success, error);
-    }
-
 };
-// Bind to window.plugins
-if(!window.plugins) {
-    window.plugins = {};
-}
-if (!window.plugins.zoom) {
-    window.plugins.zoom = zoom;
-}
 
-module.exports = zoom;
+ZoomUs.prototype.initialize = function(appKey, appSecret, success, error) {
+        callNativeFunction('initialize', [appKey, appSecret], success, error);
+};
+
+ZoomUs.prototype.login = function(username, password, success, error) {
+        callNativeFunction('login', [username, password], success, error);
+};
+
+ZoomUs.prototype.logout = function(success, error) {
+        callNativeFunction('logout', [], success, error);
+};
+
+ZoomUs.prototype.isLoggedIn = function(success, error) {
+        callNativeFunction('isLoggedIn', [], success, error);
+};
+
+
+ZoomUs.prototype.joinMeeting = function(meetingNo, meetingPassword, displayName, options, success, error) {
+        callNativeFunction('joinMeeting', [meetingNo, meetingPassword, displayName, options], success, error);
+};
+
+ZoomUs.prototype.startMeetingWithZAK = function(meetingNo, displayName, zoomToken, zoomAccessToken, userId, options, success, error) {
+        callNativeFunction('startMeeting', [meetingNo, displayName, zoomToken, zoomAccessToken, userId, options], success, error);
+};
+
+ZoomUs.prototype.startMeeting = function(meetingNo, options, success, error) {
+        callNativeFunction('startMeeting', [meetingNo, "", "", "", "", options], success, error);
+};
+
+ZoomUs.prototype.startInstantMeeting: function(options, success, error) {
+        callNativeFunction('startInstantMeeting', [options], success, error);
+};
+
+ZoomUs.prototype.setLocale = function(languageTag, success, error) {
+        callNativeFunction('setLocale', [languageTag], success, error);
+};
+
+
+// Installation constructor that binds to window
+ZoomUs.install = function() {
+	if (!window.plugins) {
+		window.plugins = {};
+	}
+	window.plugins.ZoomUs = new ZoomUs();
+	return window.plugins.ZoomUs;
+};
+cordova.addConstructor(ZoomUs.install);
